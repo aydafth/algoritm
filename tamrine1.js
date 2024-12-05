e817348ced709968af21fec172fff123c0073b62
aydafth <aydafth81@gmail.com>
2024-12-05 22:08:26 +0330

    Add files via upload

---
+++ tamrine1.js
@@ -0,0 +1,13 @@
+let a = 10
+let b = 6
+let c = 7
+let d = 2
+
+function multiply (a,b,c,d) {
+    if (a> c + d) {
+        return a*c
+    }else{
+        return b*d
+    }
+}
+console.log(multiply(a,b,c,d));
